8425168a1a5f872f7a821e1358e2be2d2dbeb0f1
Malte Dömer <maltedoemer@gmail.com>
2022-10-21 15:43:44 +0200

    started with docx exporter

--- src/main/java/qrbillius/qrbill/DocxExporter.java
+++ src/main/java/qrbillius/qrbill/DocxExporter.java
@@ -1,9 +1,17 @@
 package qrbillius.qrbill;
 
+import net.codecrete.qrbill.generator.GraphicsFormat;
+import net.codecrete.qrbill.generator.OutputSize;
+import net.codecrete.qrbill.generator.QRBill;
+
+import org.docx4j.dml.wordprocessingDrawing.Inline;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import qrbillius.SettingsManager;
+import org.docx4j.openpackaging.parts.WordprocessingML.BinaryPartAbstractImage;
+import org.docx4j.wml.Drawing;
+import org.docx4j.wml.ObjectFactory;
+import org.docx4j.wml.P;
+import org.docx4j.wml.R;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -21,14 +29,38 @@ public class DocxExporter extends AbstractExporter {
         try {
             var wordPackage = WordprocessingMLPackage.createPackage();
             var mainDocumentPart = wordPackage.getMainDocumentPart();
-            mainDocumentPart.addStyledParagraphOfText("Title", "Hello World!");
-            mainDocumentPart.addParagraphOfText("Welcome");
+
+            var info = bills.get(0);
+            var data = createImageData(info);
+
+            var imagePart = BinaryPartAbstractImage.createImagePart(wordPackage, data);
+            var inline = imagePart.createImageInline("", "QR Bill", 1, 2, false);
+            var paragraph = addImageToParagraph(inline);
+            mainDocumentPart.getContent().add(paragraph);
 
             try (var stream = new FileOutputStream(file)) {
                 wordPackage.save(stream);
             }
-        } catch (Docx4JException e) {
+        } catch (IOException e) {
+            throw e;
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private byte[] createImageData(QRBillInfo info) {
+        var bill = QRBillGenerator.createBill(info, config, GraphicsFormat.PNG, OutputSize.QR_BILL_ONLY);
+        return QRBill.generate(bill);
+    }
+
+    private static P addImageToParagraph(Inline inline) {
+        ObjectFactory factory = new ObjectFactory();
+        P p = factory.createP();
+        R r = factory.createR();
+        p.getContent().add(r);
+        Drawing drawing = factory.createDrawing();
+        r.getContent().add(drawing);
+        drawing.getAnchorOrInline().add(inline);
+        return p;
     }
 }
