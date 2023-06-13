8c0868fd71dff8cfeaece88992ad132778496100
Malte Dömer <maltedoemer@gmail.com>
2023-06-13 13:50:29 +0200

    fixed the xlsx input parser to be more consistent

--- src/main/java/qrbillius/qrbill/XLSXInputParser.java
+++ src/main/java/qrbillius/qrbill/XLSXInputParser.java
@@ -1,5 +1,6 @@
 package qrbillius.qrbill;
 
+import ch.rabanti.nanoxlsx4j.Address;
 import ch.rabanti.nanoxlsx4j.Workbook;
 import ch.rabanti.nanoxlsx4j.Worksheet;
 
@@ -53,9 +54,11 @@ public class XLSXInputParser implements InputParser {
 
         var startRow = sheet.getFirstDataRowNumber();
         var endRow = sheet.getLastDataRowNumber();
-        var totalRows = endRow - startRow;
 
-        var iterator = new RowIterator(totalRows, startRow, sheet);
+        var startColumn = sheet.getFirstDataColumnNumber();
+        var endColumn = sheet.getLastDataColumnNumber();
+
+        var iterator = new RowIterator(startRow, endRow, startColumn, endColumn, sheet);
         this.rowIterator = iterator;
 
         return iterator;
@@ -63,37 +66,53 @@ public class XLSXInputParser implements InputParser {
 
     private static class RowIterator implements Iterator<List<String>> {
 
-        private final int totalRows;
-
         private final int startRow;
+        private final int endRow;
+
+        private final int startColumn;
+        private final int endColumn;
+
         private final Worksheet sheet;
 
-        private int currentRow = 0;
+        private int currentRow;
 
-        public RowIterator(int totalRows, int startRow, Worksheet sheet) {
-            this.totalRows = totalRows;
-            this.startRow = startRow + 1;
+        public RowIterator(int startRow, int endRow, int startColumn, int endColumn, Worksheet sheet) {
+            this.startRow = startRow;
+            this.endRow = endRow;
+            this.startColumn = startColumn;
+            this.endColumn = endColumn;
+            // Note: the first row is automatically skipped
+            this.currentRow = startRow + 1;
             this.sheet = sheet;
-
         }
 
 
         @Override
         public boolean hasNext() {
-            return currentRow < totalRows;
+            return currentRow <= endRow;
         }
 
         @Override
         public List<String> next() {
-            var row = sheet.getRow(startRow + currentRow);
-            currentRow++;
-
+            var allCells = sheet.getCells();
             List<String> result = new ArrayList<>();
-            row.forEach(cell -> {
-                var value = cell.getValue();
-                var text = value == null ? "" : value.toString();
-                result.add(text);
-            });
+
+            for (int column = startColumn; column <= endColumn; column++) {
+                var address = new Address(column, currentRow).getAddress();
+                if (allCells.containsKey(address)) {
+                    // we have a cell that might contain data.
+                    var cell = allCells.get(address);
+                    var value = cell.getValue();
+                    var text = value == null ? "" : value.toString();
+                    result.add(text);
+                } else {
+                    // this is the same as if the cell is empty.
+                    result.add("");
+                }
+            }
+
+            // increment the current row for the next iteration
+            currentRow++;
 
             // TODO: if every cell is empty or whitespace only we could theoretically skip it.
             if (result.stream().allMatch(String::isBlank) && hasNext()) {
